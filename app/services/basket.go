fcfa17a8aea60861c8a08d85705934f45e158769
Arvydas Vapsva <arvydas.vapsva@rocket-internet.de>
2017-05-11 10:40:43 +0200

    Update basket.go

--- app/services/basket.go
+++ app/services/basket.go
@@ -14,6 +14,11 @@ func AddProduct(basketId string, Product models.Product) bool {
 	revel.INFO.Printf("Response from the backend:\n", resp.RawResponse)
 	repositories.StoreBasketItem(basketId, Product)
 
-	// nicolo: usually the idiomatic way to fail in go is to return an error
+	// nicolo: 
+	// usually the idiomatic way to fail in go is to return an error
+	// moreover, at that level, i like to distinguish between system errors and business errors
+	// so what I usually do, I return a tuple
+	// func AddProduct(p Product) (sys, business error)
+	// but feel free to return just one if you don't like ti
 	return resp.Status() == "200 OK"
 }
